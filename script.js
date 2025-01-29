7740403d8e59fe811ed3dd5abda5974d5e1d55ab
mouloud78 <mouloudouldali78@gmail.com>
2025-01-29 12:59:33 -0500

    add banner

---
+++ script.js
@@ -0,0 +1,19 @@
+// Navigation
+
+const nav = document.querySelector(".main-nav");
+const navToggler = document.querySelector(".nav-toggler");
+const togglerImg = navToggler.querySelector("img");
+const navLinks = document.querySelectorAll(".main-nav a");
+
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav() {
+  nav.classList.toggle("active");
+  if (togglerImg.src.includes("hamburger")) {
+    togglerImg.src = "ressources/cross.svg";
+    navToggler.ariaExpanded = "true";
+  } else {
+    togglerImg.src = "ressources/hamburger.svg";
+    navToggler.ariaExpanded = "false";
+  }
+}
